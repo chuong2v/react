90c3370d9867e3cb871f00e0719b00f5af6e0691
chuong2v <chuong.2v@gmail.com>
2016-12-15 00:29:23 +0700

    Calculator

---
+++ Calculator/index.android.js
@@ -0,0 +1,114 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class Calculator extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <View style={styles.row}>
+          <View style={styles.column}>
+            <Text style={styles.number}>1</Text>
+          </View>
+
+          <View style={styles.column}>
+            <Text style={styles.number}>2</Text>
+            <Text style={styles.characters}>ABC</Text>
+          </View>
+          
+          <View style={styles.column}>
+            <Text style={styles.number}>3</Text>
+            <Text style={styles.characters}>DEF</Text>
+          </View>
+        </View>
+        
+        <View style={styles.row}>
+          <View style={styles.column}>
+            <Text style={styles.number}>4</Text>
+            <Text style={styles.characters}>GHI</Text>
+          </View>
+
+          <View style={styles.column}>
+            <Text style={styles.number}>5</Text>
+            <Text style={styles.characters}>JKL</Text>
+          </View>
+          
+          <View style={styles.column}>
+            <Text style={styles.number}>6</Text>
+            <Text style={styles.characters}>MNO</Text>
+          </View>
+        </View>
+        
+        <View style={styles.row}>
+          <View style={styles.column}>
+            <Text style={styles.number}>7</Text>
+            <Text style={styles.characters}>PQR</Text>
+          </View>
+
+          <View style={styles.column}>
+            <Text style={styles.number}>8</Text>
+            <Text style={styles.characters}>STUV</Text>
+          </View>
+          
+          <View style={styles.column}>
+            <Text style={styles.number}>9</Text>
+            <Text style={styles.characters}>WXYZ</Text>
+          </View>
+        </View>
+        
+        <View style={styles.row}>
+          <View style={styles.column}></View>
+           <View style={styles.column}>
+            <Text style={styles.number}>0</Text>
+            <Text style={styles.characters}>@#$%&*</Text>
+          </View>
+          <View style={styles.column}></View>
+        </View>
+      
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    flexDirection: 'column'
+  },
+  row: {
+    fontSize: 20,
+    textAlign: 'center',
+    flex: 1,
+    flexDirection: 'row'
+  },
+  column: {
+    textAlign: 'center',
+    color: '#333333',
+    flex: 1,
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  number:{
+    fontSize: 48
+  },
+  characters: {
+    fontSize: 24
+  }
+});
+
+AppRegistry.registerComponent('Calculator', () => Calculator);
